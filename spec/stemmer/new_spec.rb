8e6c5fa321f15247d380967304972ccea7cbf019
Gibheer <gibheer@gmail.com>
2011-06-10 15:29:31 +0200

    remove the abstract stemmer class, because we only need an interface

--- spec/stemmer/new_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-
-describe "Stemmer#new" do
-  it "creates a new stemmer" do
-    s = Polecat::Stemmer.new
-    s.class.should be(Polecat::Stemmer)
-  end
-end
